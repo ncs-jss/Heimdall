7a0a475a283f4c51a00fc67ce742915b62a00c07
prakharchoudhary <prakhar13.cool@gmail.com>
2017-04-06 18:02:53 +0530

    models ready

--- attendence/models.py
+++ attendence/models.py
@@ -5,10 +5,31 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Attendance(models.Model):
+class LabStatus(models.Model):
+	class Meta:
+		verbose_name_plural="Lab Status"
+	created_on = models.DateField(auto_now_add=True)
+	DayNo = models.IntegerField(default=0)
+	status = models.BooleanField(default=True)
+	message = models.CharField(max_length=140, blank=True, null=True)
+
+class DailyAttendance(models.Model):
+	
+	STATUS_CHOICES = (
+    (1, " "),
+    (2, "Present"),
+    (3, "Absent"),
+    (4, "On Leave")
+	)
 	user = models.ForeignKey(User)
-	total_days = models.IntegerField(default=0)
+	date = models.DateField(auto_now_add=True)
+	status = models.IntegerField(choices=STATUS_CHOICES, default=1)
+
+class UserAttendence(models.Model):
+
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	present_days = models.IntegerField(default=0)
 	absent_days = models.IntegerField(default=0)
-	reason = models.TextAreaField(null=True)
-	informed = models.CharField(max_length=40)
+	on_leave = models.IntegerField(default=0)
+	# another field to be added that records the no of leaves in the ongoing month and 
+	# becomes 0 at the beginning of new month.
